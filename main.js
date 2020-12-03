c805101b6ac8634430c24d7000714849171a1c5b
eerecius <35268255+eerecius@users.noreply.github.com>
2020-12-03 14:25:16 -0800

    Add files via upload

--- main.js
+++ main.js
@@ -1,10 +1,25 @@
-let punctNum = Math.random();
-let punct;
-if (punctNum < (1/3)) {
-	punct = "???";
-} else if (punctNum >= (1/3) && punctNum < (2/3)) {
-	punct = "!!!";
-} else {
-	punct = "...";
+function changePunct(){
+	let punctNum = Math.random();
+	let punct;
+	if (punctNum < (1/3)) {
+		punct = "???";
+	} else if (punctNum >= (1/3) && punctNum < (2/3)) {
+		punct = "!!!";
+	} else {
+		punct = "...";
+	}
+	document.getElementById("punctuation").innerHTML = punct;
 }
-document.getElementById("punctuation").innerHTML = punct;
+
+function hiss(){
+	document.getElementsByClassName("wpngrl-hiss")[0].style.display="block";
+	document.getElementsByClassName("wpngrl-img")[0].style.display="none";
+}
+
+function unHiss(){
+	document.getElementsByClassName("wpngrl-hiss")[0].style.display="none";
+	document.getElementsByClassName("wpngrl-img")[0].style.display="block";
+}
+
+changePunct()
+
