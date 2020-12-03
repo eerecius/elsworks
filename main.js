eba632ad3bcc105aaf088efba25820833d14242a
eerecius <35268255+eerecius@users.noreply.github.com>
2020-12-03 15:33:35 -0800

    Add files via upload

--- main.js
+++ main.js
@@ -1,16 +1,3 @@
-function changePunct(){
-	let punctNum = Math.random();
-	let punct;
-	if (punctNum < (1/3)) {
-		punct = "???";
-	} else if (punctNum >= (1/3) && punctNum < (2/3)) {
-		punct = "!!!";
-	} else {
-		punct = "...";
-	}
-	document.getElementById("punctuation").innerHTML = punct;
-}
-
 function hiss(){
 	document.getElementsByClassName("wpngrl-hiss")[0].style.display="block";
 	document.getElementsByClassName("wpngrl-img")[0].style.display="none";
