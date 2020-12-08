9634f73e4311dd422ac26915fd974f639c58da52
admin <eerecius@gmail.com>
2020-12-07 20:39:30 -0800

    gallery part 1

--- main.js
+++ main.js
@@ -7,3 +7,19 @@ function unHiss(imgDefault, imgHover){
 	document.getElementsByClassName(imgHover)[0].style.display="none";
 	document.getElementsByClassName(imgDefault)[0].style.display="block";
 }
+
+function galleryMouseover(num){
+	document.getElementsByClassName("gallery-text")[num].style.letterSpacing="3px";
+}
+
+function galleryMouseout(num) {
+	document.getElementsByClassName("gallery-text")[num].style.letterSpacing="-3px";
+}
+
+function showGallery(imgSet) {
+	document.getElementsByClassName("gallery")[0].style.display="block";
+}
+
+function hideGallery() {
+	document.getElementsByClassName("gallery")[0].style.display="none";
+}
