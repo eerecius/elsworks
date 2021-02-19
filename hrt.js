ef7ed768930f6d23fed11424f15875b36b71c180
admin <eerecius@gmail.com>
2021-02-18 16:31:58 -0800

    rebrand

--- hrt.js
+++ hrt.js
@@ -1,5 +1,5 @@
 function upDate() {
-	let dateStarted = new Date(2020, 04, 06);
+	let dateStarted = new Date(2020, 03, 06);
 	let now = new Date();
 	document.getElementById("time-since").innerHTML = (now-dateStarted);
 	setTimeout(upDate, 1);
