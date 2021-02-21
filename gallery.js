f00ca0d862795c340a44ed841fea8bb22dc229ed
admin <eerecius@gmail.com>
2021-02-21 14:25:13 -0800

    added gallery left right buttons (temporary styling)

--- gallery.js
+++ gallery.js
@@ -1,11 +1,78 @@
-function enlarge(piece){
-	document.getElementById("gallery-active").src = piece.src;
-	document.getElementById("gallery-text").innerHTML = piece.title.replace(" - ", "<br>");
+let shownPiece = document.getElementById("gallery-active");
+let shownText = document.getElementById("gallery-text");
+
+let newerArrow = document.getElementById("newer-arrow");
+let olderArrow = document.getElementById("older-arrow");
+
+let activePiece;
+let nextPiece;
+let previousPiece;
+
+function updateArrows() {
+	if (previousPiece === null) {
+		newerArrow.disabled = true;
+	} else {
+		newerArrow.disabled = false;
+	}
+	if (nextPiece === null) {
+		olderArrow.disabled = true;
+	} else {
+		olderArrow.disabled = false;
+	}
+}
+
+function enlarge(piece){	
+	activePiece = piece;
+	
+	shownPiece.src = piece.src;
+	shownText.innerHTML = piece.title.replace(" - ", "<br>");
+	
+	if (nextPiece !== null) {
+		nextPiece = piece.nextElementSibling;
+	}
+	if (previousPiece !== null) {
+		previousPiece = piece.previousElementSibling;
+	}
+	
 	document.getElementById("content").style.display = "none";
 	document.getElementById("gallery-container").style.display = "block";
+	
+	updateArrows();
 }
 
 function ensmall() {
 	document.getElementById("content").style.display = "block";
 	document.getElementById("gallery-container").style.display = "none";
+}
+
+function scrollToRight() {	
+	if (nextPiece !== null) {
+		shownPiece.src = nextPiece.src;
+		shownText.innerHTML = nextPiece.title.replace(" - ", "<br>");
+	
+		activePiece = nextPiece;
+	
+		previousPiece = activePiece.previousElementSibling;
+		nextPiece = activePiece.nextElementSibling;
+	} else {
+		return;
+	}
+	
+	updateArrows();
+}
+
+function scrollToLeft() {
+	if (previousPiece !== null) {
+		shownPiece.src = previousPiece.src;
+		shownText.innerHTML = previousPiece.title.replace(" - ", "<br>");
+		
+		activePiece = previousPiece;
+		
+		previousPiece = activePiece.previousElementSibling;
+		nextPiece = activePiece.nextElementSibling;
+	} else {
+		return;
+	}
+	
+	updateArrows();
 }
