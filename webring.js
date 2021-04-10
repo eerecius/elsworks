59681efaba15137b347c3980d6d2b7b80e2d96f6
admin <eerecius@gmail.com>
2021-04-09 22:29:13 -0700

    decringed

---
+++ webring.js
@@ -0,0 +1,8 @@
+function showSitelist() {
+	document.getElementById("sitelist").style.display = "block";
+	console.log("shown element sitelist");
+}
+
+function closeSitelist() {
+	document.getElementById("sitelist").style.display = "none";
+}
