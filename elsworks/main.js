90d5e62e76cd339ec5b5bf5b7ef49e06b021a30e
eerecius <35268255+eerecius@users.noreply.github.com>
2021-11-10 17:28:14 -0800

    Format overhaul

---
+++ elsworks/main.js
@@ -0,0 +1,11 @@
+//old wpngrl hiss functions, preserved in case of need
+
+//function hiss(imgDefault, imgHover){
+//	document.getElementsByClassName(imgHover)[0].style.display="block";
+//	document.getElementsByClassName(imgDefault)[0].style.display="none";
+//}
+
+//function unHiss(imgDefault, imgHover){
+//	document.getElementsByClassName(imgHover)[0].style.display="none";
+//	document.getElementsByClassName(imgDefault)[0].style.display="block";
+//}
